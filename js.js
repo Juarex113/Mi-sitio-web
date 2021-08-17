d979dd02104684cdfc77c5b699c2016f70251e43
Juarex113 <81634733+Juarex113@users.noreply.github.com>
2021-08-17 12:44:41 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,88 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.nav', {
+        duration: 3000,
+        origin: 'botton',
+        distance:'-100px'
+    });
+    sr.reveal('.subtitle2', {
+        duration: 3000,
+        origin: 'botton',
+        distance:'-100px'
+    });
+    sr.reveal('.habilidades', {
+        duration: 3000,
+        origin: 'top',
+        distance:'-100px'
+    });
+    sr.reveal('.proyects__item', {
+        duration: 3000,
+        origin: 'botton',
+        distance:'-100px'
+    });
+    sr.reveal('.hero__title', {
+        duration: 3000,
+        origin: 'left',
+        distance:'-100px'
+    });
+    sr.reveal('.hero__subtitle', {
+        duration: 3000,
+        origin: 'left',
+        distance:'-80px'
+    });
+    sr.reveal('.hero__cta', {
+        duration: 3000,
+        origin: 'left',
+        distance:'-60px'
+    });
+    sr.reveal('.presentation__picture', {
+        duration: 2000,
+        origin: 'left',
+        distance:'-60px'
+    });
+//----
+    sr.reveal('.subtitle', {
+        duration: 1000,
+        origin: 'right',
+        distance:'-80px'
+    });
+    sr.reveal('.presentation__copy', {
+        duration: 1000,
+        origin: 'right',
+        distance:'-80px'
+    });
+    sr.reveal('.presentation__cta', {
+        duration: 1000,
+        origin: 'right',
+        distance:'-80px'
+    });
+    sr.reveal('.about__paragraph', {
+        duration: 1000,
+        origin: 'top',
+        distance:'-80px'
+    });
+    sr.reveal('table', {
+        duration: 1000,
+        origin: 'top',
+        distance:'-80px'
+    });
+    sr.reveal('habilidades__title', {
+        duration: 1000,
+        origin: 'top',
+        distance:'-80px'
+    });
+    sr.reveal('projects', {
+        duration: 1000,
+        origin: 'top',
+        distance:'-80px'
+    });
+    sr.reveal('.footer__contact', {
+        duration: 1000,
+        origin: 'top',
+        distance:'-80px'
+    });
+    
+
+
+
+
